249f156289aedbe93d82a5e40b9c235057f22548
Kth Nwl <nwal.kahia@gmail.com>
2021-07-13 15:06:12 +0200

    styling index.html

--- Front-end/index.js
+++ Front-end/index.js
@@ -14,21 +14,24 @@ fetch(url)
   .then((resp) => resp.json())
   .then(function (data) {
     let furnitures = data
-    console.log(furnitures)
     //Pour chaque furniture dans furnitures, on créer un élément de liste qui retourne <img>, "name" et "price" correspondant
     return furnitures.map(function (furniture) {
       let li = createNode('li')
+      let a = createNode('a')
+      a.href="produit.html"
       let img = createNode('img')
+      img.className = "img-art"
       let span = createNode('span')
       img.src = furniture.imageUrl
       span.innerHTML = `${furniture.name} prix : ${furniture.price} €`
-      append(li, img)
-      append(li, span)
+      append(li, a)
+      append(a, img)
+      append(a, span)
       append(ul, li)
-      span.style.fontWeight = 'bold'
     })
   })
   //on enregistre les erreurs potentielles sur la console
   .catch(function (error) {
     console.log(error)
   })
+ 
