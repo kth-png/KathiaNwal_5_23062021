951680dd45960d2bba6b3f771ebafa1e68ab980a
Kth Nwl <nwal.kahia@gmail.com>
2021-07-24 04:33:20 +0200

    create page 2 and set product datas display in product.js

--- Front-end/index.js
+++ Front-end/index.js
@@ -12,7 +12,7 @@ function redirect() {
 }
 const page = 'produit.html'
 const ul = document.getElementById('furnitures')
-const url = 'http://localhost:3000/api/furniture'
+let url = 'http://localhost:3000/api/furniture'
 
 //Call API pour récupérer tous les articles
 
@@ -23,120 +23,15 @@ fetch(url)
     //Boucle de récupération des données
     for (furniture of furnitures) {
       //création des éléments pour afficher les données
+      let link = createNode("a")
+      link.href = `${page}?id=${furniture._id}`
+      link.classList.add('fig-width')
+      append(ul, link)
       let figure = createNode('figure')
-      figure.classList.add('fig-width')
-      append(ul, figure)
-      let given_id = furniture._id
-      figure.addEventListener('click', function (e) {
-        urlId = `http://localhost:3000/api/furniture/${given_id}`
-
-        //CALL API pour récupérer chaque article individuellement
-        fetch(urlId)
-          .then((resp) => resp.json())
-          .then(function (data) {
-            //Création des éléments pour afficher les données récupérées
-            let container = document.querySelector('#main')
-            let artContainer = createNode('main')
-            let body = document.querySelector('body')
-            //Remplacement d'un élément de la page d'acceuil par les nouveaux éléments crées
-            body.replaceChild(artContainer, container)
-
-            let div = createNode('div')
-            append(artContainer, div)
-            div.classList.add('container', 'mt-5', 'center')
-
-            let divTitle = createNode('div')
-            divTitle.classList.add('divTitle')
-            append(div, divTitle)
-
-            let title = createNode('h2')
-            append(divTitle, title)
-            title.innerHTML = data.name
-
-            let idNum = createNode('span')
-            append(divTitle, idNum)
-            idNum.innerHTML = `ID Produit: ${data._id}`
-
-            let divImg = createNode('div')
-            divImg.classList.add('divImg', 'artMedia')
-            append(div, divImg)
-
-            let img = createNode('img')
-            img.classList.add('img-display')
-            append(divImg, img)
-            img.src = data.imageUrl
-            img.height = '350'
-            img.width = '350'
-
-            let divPers = createNode('div')
-            divPers.classList.add('divPers')
-            append(divImg, divPers)
-
-            let artDes = createNode('p')
-            append(divPers, artDes)
-            artDes.innerHTML = data.description
-
-            let divFin = createNode('div')
-            divFin.classList.add('select')
-            append(divPers, divFin)
-            
-            let label = createNode('label')
-            label.setAttribute('for', 'perso')
-            label.innerHTML = 'Choisissez une finition'
-            append(divFin, label)
-
-            let artPers = createNode('select')
-            artPers.setAttribute('id', 'perso')
-            artPers.setAttribute('name', 'varnish')
-            let artPersArr = data.varnish
-            append(divFin, artPers)
-            //Boucle d'implémentation des options de finition
-            for (let i = 0; i < artPersArr.length; i++) {
-              let persChoice = createNode('option')
-              append(artPers, persChoice)
-              persChoice.innerHTML = artPersArr[i]
-            }
-
-            let divQty = createNode('div')
-            divQty.classList.add('select')
-            append(divPers, divQty)
-
-            let labelQty = createNode('label')
-            labelQty.setAttribute('for', 'qty')
-            labelQty.innerHTML = 'Quantité'
-            append(divQty, labelQty)
-
-            let choice = createNode('select')
-            choice.setAttribute('id', 'choice')
-            choice.setAttribute('name', 'choices')
-            append(divQty, choice)
-            //Boucle de création des options de quantité
-            for (let i = 1; i <= 10; i += 1) {
-              let qtyArr = i
-              let qtyChoice = createNode('option')
-              append(choice, qtyChoice)
-              qtyChoice.innerHTML = qtyArr
-            }
-
-            let divPrice = createNode('div')
-            divPrice.classList.add('divPrice')
-            append(divPers, divPrice)
-
-            let price = createNode('h3')
-            append(divPrice, price)
-            //Affichage du prix en euro, arrondi à l'entier supérieur
-            price.innerHTML = `${Math.ceil(data.price / 1000)}€`
-
-            let unit = createNode('span')
-            append(divPrice, unit)
-            unit.classList.add('unit')
-            unit.innerHTML = "Prix à l'unité"
-          })
-          .catch(function (error) {
-            alert(error)
-          })
-      })
-
+      
+      figure.setAttribute("id",`"${furniture._id}"`)
+      append(link, figure)
+      
       let img = createNode('img')
       img.classList.add('card-img-top')
       append(figure, img)
--- Front-end/product.js
+++ Front-end/product.js
@@ -1,29 +1,113 @@
-const container = document.querySelector('#article')
+//récupération de la chaine de caractère dans l'url
+const queryString = window.location.search
 
-fetch(urlId)
-.then((resp) => resp.json())
-.then(function(data){
-  let div = createNode('div')
-  append(container, div)
+//extraction de l'id
+const urlSearchParams = new URLSearchParams(queryString)
+const myId = urlSearchParams.get('id')
+//test récupération id
+//console.log(myId)
 
-  let img = createNode('img')
-  append(div, img)
-  img.src = data.imageUrl
+//récupération de l'URL de l'API
 
-  let title = createNode('h1')
+const productUrl = `http://localhost:3000/api/furniture/${myId}`
+//console.log(productUrl)
 
-  let idNum = createNode('span')
+//récupération des données du produit sélectionné
+fetch(productUrl)
+          .then((resp) => resp.json())
+          .then(function (data) {
+            //Création des éléments pour afficher les données récupérées
+            let article = document.querySelector('#article')
+            article.classList.add('container', 'mt-5', 'center')
 
-  let artDes = createNode('p')
+            let divTitle = createNode('div')
+            divTitle.classList.add('divTitle')
+            append(article, divTitle)
 
-  let choice = createNode('select')
+            let title = createNode('h2')
+            append(divTitle, title)
+            title.innerHTML = data.name
 
-  let qtyChoice = createNode('option')
+            let divImg = createNode('div')
+            divImg.classList.add('divImg', 'artMedia')
+            append(article, divImg)
 
-  let artPers = createNode('select')
+            let img = createNode('img')
+            img.classList.add('img-display')
+            append(divImg, img)
+            img.src = data.imageUrl
+            img.height = '350'
+            img.width = '350'
 
-  let persChoice = createNode('option')
-})
-.catch( function (error){
-alert('error')
-})
+            let divPers = createNode('div')
+            divPers.classList.add('divPers')
+            append(divImg, divPers)
+
+            let artDes = createNode('p')
+            append(divPers, artDes)
+            artDes.innerHTML = data.description
+
+            let divFin = createNode('div')
+            divFin.classList.add('select')
+            append(divPers, divFin)
+            
+            let label = createNode('label')
+            label.setAttribute('for', 'perso')
+            label.innerHTML = 'Choisissez une finition'
+            append(divFin, label)
+
+            let artPers = createNode('select')
+            artPers.setAttribute('id', 'perso')
+            artPers.setAttribute('name', 'varnish')
+            let artPersArr = data.varnish
+            append(divFin, artPers)
+
+            //Boucle d'implémentation des options de finition
+            for (let i = 0; i < artPersArr.length; i++) {
+              let persChoice = createNode('option')
+              append(artPers, persChoice)
+              persChoice.innerHTML = artPersArr[i]
+            }
+
+            let divQty = createNode('div')
+            divQty.classList.add('select')
+            append(divPers, divQty)
+
+            let labelQty = createNode('label')
+            labelQty.setAttribute('for', 'qty')
+            labelQty.innerHTML = 'Quantité'
+            append(divQty, labelQty)
+
+            let choice = createNode('select')
+            choice.setAttribute('id', 'choice')
+            choice.setAttribute('name', 'choices')
+            append(divQty, choice)
+
+            //Boucle de création des options de quantité
+            for (let i = 1; i <= 10; i += 1) {
+              let qtyArr = i
+              let qtyChoice = createNode('option')
+              append(choice, qtyChoice)
+              qtyChoice.innerHTML = qtyArr
+            }
+
+            let divPrice = createNode('div')
+            divPrice.classList.add('divPrice')
+            append(divPers, divPrice)
+
+            let price = createNode('h3')
+            append(divPrice, price)
+
+            //Affichage du prix en euro, arrondi à l'entier supérieur
+            price.innerHTML = `${Math.ceil(data.price / 1000)}€`
+
+            let unit = createNode('span')
+            append(divPrice, unit)
+            unit.classList.add('unit')
+            unit.innerHTML = "Prix à l'unité"
+          })
+          .catch(function (error) {
+          alert(error)
+          })
+        
+
